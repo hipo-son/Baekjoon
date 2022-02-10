3cd50ab2dee581c9ac0dcc62d447cb882fe5a44a
hipo-son <hiposon214@gmail.com>
2022-02-11 05:55:17 +0900

    success

---
+++ step_by_step/level_1/9_10869.py
@@ -0,0 +1,7 @@
+num = input().split()
+
+print(int(num[0]) + int(num[1]))
+print(int(num[0]) - int(num[1]))
+print(int(num[0]) * int(num[1]))
+print(int(num[0]) // int(num[1]))
+print(int(num[0]) % int(num[1]))
