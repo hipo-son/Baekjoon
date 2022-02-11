1d1bed964e5950742ba4acec3c0fc828a9a5eec4
hipo-son <hiposon214@gmail.com>
2022-02-11 23:32:52 +0900

    success

---
+++ step_by_step/level_7/5_1157.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+from collections import Counter
+while True: # 굳이 예외는 안해도 됬음 , 런타임 떠서 시도해봤음
+    try:
+        word = input().rstrip().upper() # 대문자 변환
+        list = Counter(word).most_common()
+        if len(list) == 1:
+            print(list[0][0])
+        else:
+            if list[0][1] == list[1][1] or len(list) == 1:
+                print('?')
+            else:
+                print(list[0][0])
+    except:
+        break
