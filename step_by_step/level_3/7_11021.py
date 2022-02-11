a75d2648643f012bac2a5c954628cb3ee53c0d28
hipo-son <hiposon214@gmail.com>
2022-02-11 13:37:32 +0900

    success

---
+++ step_by_step/level_3/7_11021.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+num = int(input().rstrip())
+
+for i in range(1, num + 1):
+    tmp = input().rstrip().split()
+    print("Case #{0}: {1}".format(i, (int(tmp[0]) + int(tmp[1]))))
