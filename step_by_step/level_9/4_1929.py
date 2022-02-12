c7bf005ca5969aafbd5223291922f188f71a32e8
hipo-son <hiposon214@gmail.com>
2022-02-12 22:58:36 +0900

    success

--- step_by_step/level_9/4_1929.py
+++ step_by_step/level_9/4_1929.py
@@ -1,10 +1,20 @@
-n=1000
+import sys
+input = sys.stdin.readline
+M, N = map(int, input().rstrip().split())
+
+n = N
 a = [False,False] + [True]*(n-1)
 primes=[]
 
-for i in range(2,n+1):
+for i in range(1,n+1):
   if a[i]:
     primes.append(i)
     for j in range(2*i, n+1, i):
         a[j] = False
-print(primes)
+
+if M % 2 == 0:
+    M += 1
+
+for num in range(M, N, 2):
+    if num in primes:
+        print(num)
