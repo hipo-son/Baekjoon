ac0647f2072206786de8f7a1a70f5796cf2f3787
hipo-son <hiposon214@gmail.com>
2022-02-12 23:17:42 +0900

    success

---
+++ step_by_step/level_9/8_3009.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+xy1 = list(map(int, input().rstrip().split()))
+xy2 = list(map(int, input().rstrip().split()))
+xy3 = list(map(int, input().rstrip().split()))
+
+result = [0,0]
+for i in range(0,2):
+    if xy1[i] == xy2[i]:
+        result[i] = xy3[i]
+    elif xy1[i] == xy3[i]:
+        result[i] = xy2[i]
+    elif xy2[i] == xy3[i]:
+        result[i] = xy1[i]
+print(result[0], result[1])
