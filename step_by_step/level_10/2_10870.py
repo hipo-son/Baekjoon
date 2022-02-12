3b4b2d1f0adbc1f12279088c5bce769376e50de3
hipo-son <hiposon214@gmail.com>
2022-02-13 00:27:32 +0900

    yet

---
+++ step_by_step/level_10/2_10870.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+def fib(n):
+        f0, f1 = 0, 1
+        f = [1] * n
+        for i in range(1,n):
+            f[i] = f0 + f1
+            f0 , f1 = f1, f[i]
+        return f
+
+n = int(input().rstrip())
+print(fib(n)[n-1])
