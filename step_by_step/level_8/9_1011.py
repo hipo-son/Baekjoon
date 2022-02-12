e6022349ba32e7ac8692a28da91a1257fa8b655b
hipo-son <hiposon214@gmail.com>
2022-02-12 20:06:50 +0900

    success

--- step_by_step/level_8/9_1011.py
+++ step_by_step/level_8/9_1011.py
@@ -3,10 +3,10 @@ input = sys.stdin.readline
 T = int(input().rstrip())
 for i in range(T):
     x, y = map(int, input().rstrip().split())
-    distance = y - x
+    distance = y - x   # 좌표간 거리
     count, total, tmp = 0, 0, 0
     i = 1
-    while distance > total:
+    while distance > total: # 공간이동시 가장 멀리 갈수 있는 total 계산 
         if tmp == 1:
             tmp = 0
             total += i
