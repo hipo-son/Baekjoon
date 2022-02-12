ac9d8bdb74b88ce347d4bf3275948641bc618ed7
hipo-son <hiposon214@gmail.com>
2022-02-12 20:30:07 +0900

    success

--- step_by_step/level_8/7_2839.py
+++ step_by_step/level_8/7_2839.py
@@ -5,9 +5,13 @@ n = int(input().rstrip())
 
 count = 0
 remainder = (n % 30)
-n_3 = list(range(3,30,5)) + list(range(6,30,5)) + list(range(9,30,5)) + list(range(12,30,5)) + list(range(0,30,5))
-print(n_3)
-if remainder in n_3 :
+n_3 = list(range(3,60,5)) + list(range(6,60,5)) + list(range(9,60,5)) + list(range(12,60,5)) + list(range(0,60,5))
+n_3_defult = list(range(3,30,5)) + list(range(6,30,5)) + list(range(9,30,5)) + list(range(12,30,5)) + list(range(0,30,5))
+n_3_list = list(map(lambda x: (x % 30), n_3))
+
+if n in set(n_3_list)-set(n_3_defult):
+    count = -1
+else:
     count += (n // 30) * 6
     if n % 5 == 0: # 5 만 있을때
         count += remainder // 5
@@ -23,7 +27,6 @@ if remainder in n_3 :
     elif n % 5 == 4: # 3이 3개
         count += 3
         count += (remainder - 9) // 5
-else:
-    count = -1
 
 print(count)
+############################### fix
