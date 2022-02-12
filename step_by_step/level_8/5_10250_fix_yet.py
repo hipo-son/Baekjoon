e8a7b13b727e162fc3a65bd75c8720dad8ef35ea
hipo-son <hiposon214@gmail.com>
2022-02-12 16:22:10 +0900

    success

--- step_by_step/level_8/5_10250_fix_yet.py
+++ step_by_step/level_8/5_10250_fix_yet.py
@@ -6,11 +6,10 @@ for i in range(T):
     H ,W, N = map(int, input().rstrip().split())
 
     YY = N % H
-    if YY == 0:
+    if YY == 0: # 꼭대기 층 일떄
         YY = H
 
     XX = N // H + 1
-    if N % H == 0:
+    if N % H == 0: # 꼭대기 층 일때
         XX -= 1
     print('%d%02d'%(YY,XX))
-################################
