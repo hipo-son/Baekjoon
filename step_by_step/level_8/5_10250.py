a697059263739b9ae0b0ed8685fea86b4947922e
hipo-son <hiposon214@gmail.com>
2022-02-12 16:13:06 +0900

    success

---
+++ step_by_step/level_8/5_10250.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+T = int(input().rstrip())
+
+for i in range(T):
+    H ,W, N = map(int, input().rstrip().split())
+
+    YY = N % H
+    if YY == 0:
+        YY = H
+
+    XX = N // H + 1
+    if N % H == 0:
+        XX -= 1
+    print('%d%02d'%(YY,XX))
