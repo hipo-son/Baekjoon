079b3ed252dee3e4916088020d789ef918cb1d95
hipo-son <hiposon214@gmail.com>
2022-02-13 12:59:00 +0900

    success

--- step_by_step/level_9/11_1002.py
+++ step_by_step/level_9/11_1002.py
@@ -5,12 +5,12 @@ import math
 T = int(input().rstrip())
 for i in range(T):
     x1, y1, r1, x2, y2, r2 = map(int, input().rstrip().split())
-    distance = math.sqrt((x1-x2)**2 + (y1-y2)**2)
-    if distance == 0 and r1 == r2 :
+    distance = math.sqrt((x1-x2)**2 + (y1-y2)**2) # 두 원의 거리
+    if distance == 0 and r1 == r2 : # 합동
         print(-1)
-    elif abs(r1-r2) == distance or r1 + r2 == distance:
+    elif abs(r1-r2) == distance or r1 + r2 == distance: # 내접, 외점 원
         print(1)
-    elif abs(r1-r2) < distance < (r1+r2) :
+    elif abs(r1-r2) < distance < (r1+r2) : # 교점이 두개일때
         print(2)
-    else:
+    else: # 접하지 않을때
         print(0)
