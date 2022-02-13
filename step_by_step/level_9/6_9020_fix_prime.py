83862bc4047089320b861d0c5d650335496b653e
hipo-son <hiposon214@gmail.com>
2022-02-14 01:45:23 +0900

    success

--- step_by_step/level_9/6_9020_fix_prime.py
+++ step_by_step/level_9/6_9020_fix_prime.py
@@ -5,7 +5,7 @@ T = int(input().rstrip())
 num = 10000 + 1
 prime = [False] * num
 
-for i in range(1, num):
+for i in range(1, num): # 범위의 1~10000 까지의 소수 만들기
     if i == 1:
         continue
     for j in range(2, int(i ** 0.5)+1 ):
@@ -17,10 +17,10 @@ for i in range(1, num):
 for i in range(T):
     N = int(input().rstrip())
     list = [False] * N
-    for j in range(1, N+1):
+    for j in range(1, N+1): # N 값 까지의 소수 만들
         if prime[j-1] :
             list[j-1] = True
-    for k in range(int(N/2), N + 1):
+    for k in range(int(N/2), N + 1): # 짝수 N에서 차이가 가장적은 소수 쌍을 찾아야하므로 N/2 부터 탐색
         if list[k-1] and list[(N - k)-1] :
             print((N - k) , k)
             break
