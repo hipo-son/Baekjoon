a1cadb1c01e6380f50ebf00a50735e67f4f16006
hipo-son <hiposon214@gmail.com>
2022-02-13 14:41:34 +0900

    yet

--- step_by_step/level_9/5_4948_yet.py
+++ step_by_step/level_9/5_4948_yet.py
@@ -1,23 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-N = 5 * 2 + 1
-sieve = [False] * N   # 중요
-for i in range(2, N * 2 + 1):
+num = 123456 * 2 + 1
+prime = [False] * num
+
+for i in range(1, num):
     if i == 1:
         continue
-    for j in range(2, int(i ** 0.5)+1):
+    for j in range(2, int(i ** 0.5)+1 ):
         if i%j==0:
             break
     else:
-        sieve[i] = True
+        prime[i-1] = True
 
-print(sieve)
-
+N = None
 while N !=0 :
     N = int(input().rstrip())
     count = 0
     for i in range(N+1, 2*N+1):
-        if i in prime:
+        if prime[i-1] :
             count += 1
     print(count)
