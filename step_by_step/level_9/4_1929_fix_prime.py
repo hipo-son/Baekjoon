57906bd7ea76765f56689c46fdba7cc217b947e3
hipo-son <hiposon214@gmail.com>
2022-02-13 13:21:24 +0900

    success

--- step_by_step/level_9/4_1929_fix_prime.py
+++ step_by_step/level_9/4_1929_fix_prime.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 M, N = map(int, input().rstrip().split())
 
 for i in range(M, N+1):
-    if i == 1: #1은 소수가 아뉘지!
+    if i == 1: #1은 소수 X
         continue
     for j in range(2, int(i** 0.5)+1 ):
         if i%j==0:
