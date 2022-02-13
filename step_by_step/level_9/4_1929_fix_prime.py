b462586f8dde1183e7ce0f2665dc742f2c48fb5f
hipo-son <hiposon214@gmail.com>
2022-02-13 13:25:50 +0900

    success

--- step_by_step/level_9/4_1929_fix_prime.py
+++ step_by_step/level_9/4_1929_fix_prime.py
@@ -5,8 +5,9 @@ M, N = map(int, input().rstrip().split())
 for i in range(M, N+1):
     if i == 1: #1은 소수 X
         continue
-    for j in range(2, int(i** 0.5)+1 ):
+    for j in range(2, int(i ** 0.5)+1 ): # 약수 판별 : 약수 판별시 모든수를 대입해 보는것 보다 제곱근을 이용해서 구하는게 빠르다. 이유: 보통 12의 약수 1,2,3,4,6,12 일떄 1*12, 2*6, 3*4 이렇게 이뤄졌으므로 
         if i%j==0:
             break
     else:
         print(i)
+################################
