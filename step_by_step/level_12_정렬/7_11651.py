1dbaa8ae5711336a21e4caed120aec59e25588d6
hipo-son <hiposon214@gmail.com>
2022-02-15 22:32:24 +0900

    success

--- step_by_step/level_12_정렬/7_11651.py
+++ step_by_step/level_12_정렬/7_11651.py
@@ -4,9 +4,9 @@ input = sys.stdin.readline
 N = int(input().rstrip())
 L = []
 for i in range(N):
-    a, b = map(int, input().rstrip().split()))
-    L.append([a, b])
+    a, b = map(int, input().rstrip().split())
+    L.append([b, a])
 L.sort()
 
 for i in L:
-    print(i[0], i[1])
+    print(i[1], i[0])
