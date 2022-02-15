2ce8a94d503a99f796ae2cd5454eb9779830234d
hipo-son <hiposon214@gmail.com>
2022-02-15 13:57:02 +0900

    success

--- step_by_step/level_11_브루트 포스/3_7568.py
+++ step_by_step/level_11_브루트 포스/3_7568.py
@@ -3,5 +3,14 @@ input = sys.stdin.readline
 
 N = int(input().rstrip())
 
+list = []
 for i in range(N):
     x, y = map(int, input().rstrip().split())
+    list.append([x,y])
+
+for i in range(N):
+    rank = 1
+    for j in range(N):
+        if list[i][0] < list[j][0] and list[i][1] < list[j][1]:
+            rank += 1
+    print(rank, end=' ')
