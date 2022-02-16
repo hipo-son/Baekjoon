c4fe5ff595cb8766cca96eae68454cd9577be750
hipo-son <hiposon214@gmail.com>
2022-02-16 16:57:51 +0900

    success

--- step_by_step/level_12_정렬/10_18870.py
+++ step_by_step/level_12_정렬/10_18870.py
@@ -3,4 +3,10 @@ input = sys.stdin.readline
 
 N = int(input().rstrip())
 
-x = list(map(int, input().rstrip().split()))
+L = list(map(int, input().rstrip().split())) # 리스트 만들기
+
+L_1 = sorted(list(set(L))) # set 으로 중복 제거, 오름차순 정렬
+dic = {L_1[i] : i for i in range(len(L_1)) } # 딕셔너리 만듬 , index를 value로
+
+for i in L: # 입력맏은 값의 value 출력
+    print(dic[i] , end=' ')
