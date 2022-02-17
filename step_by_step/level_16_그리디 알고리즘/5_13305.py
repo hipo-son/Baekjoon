3b48d684be6b785516b52f2acbdcc1e859aa4a0e
hipo-son <hiposon214@gmail.com>
2022-02-18 01:58:47 +0900

    success

--- step_by_step/level_16_그리디 알고리즘/5_13305.py
+++ step_by_step/level_16_그리디 알고리즘/5_13305.py
@@ -5,11 +5,11 @@ N = int(input().rstrip())
 road = list(map(int, input().rstrip().split()))
 city = list(map(int, input().rstrip().split()))
 
-total = 0
-min_oil = city[0]
+total = 0 # 총비용
+min_oil = city[0] #처음 비용
 for i in range(N-1):
-    if city[i] <= min_oil:
+    if city[i] <= min_oil: #도시의 기름값이 최소인가?
         min_oil = city[i]
-    total += road[i] * min_oil
+    total += road[i] * min_oil # 해당도시와 다음도시의 거리 
 
 print(total)
