113e881e77622585652693ec44d5eb00a77223d8
hipo-son <hiposon214@gmail.com>
2022-02-18 00:17:29 +0900

    success

--- step_by_step/level_16_그리디 알고리즘/1_11047.py
+++ step_by_step/level_16_그리디 알고리즘/1_11047.py
@@ -4,11 +4,11 @@ input = sys.stdin.readline
 N, M = map(int, input().rstrip().split())
 Li = [int(input().rstrip()) for _ in range(N)]
 
-Li.reverse()
+Li.reverse() # 큰 순서대로
 cnt = 0
 
 for i in Li:
-    if M >= i :
+    if M >= i : # 나눌수 있는 수는 빼준다
         cnt += M // i
         M %= i
 print(cnt)
--- step_by_step/level_16_그리디 알고리즘/2_1931.py
+++ step_by_step/level_16_그리디 알고리즘/2_1931.py
@@ -3,12 +3,12 @@ input = sys.stdin.readline
 
 N = int(input().rstrip())
 Li = []
-for i in range(N):
+for i in range(N): # [a, b] 형태로 저장
     a, b = map(int, input().rstrip().split())
     Li.append([a, b])
 
-Li.sort(key = lambda a: a[0])
-Li.sort(key = lambda a: a[1])
+Li.sort(key = lambda a: a[0]) # 시작시간이 동일시 일때는 시작시간이 빨라야 한다 (조건 2)
+Li.sort(key = lambda a: a[1]) # 회의의 종료시간이 빨라야 한다 (조건 1)
 
 cnt , last = 0, 0
 print(Li)
