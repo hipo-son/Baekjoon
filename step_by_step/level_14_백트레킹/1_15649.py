e10200d5c1b1f84f2184eab2516e7587a94ad7f5
hipo-son <hiposon214@gmail.com>
2022-02-18 02:12:22 +0900

    success

--- step_by_step/level_14_백트레킹/1_15649.py
+++ step_by_step/level_14_백트레킹/1_15649.py
@@ -5,16 +5,16 @@ N, M = map(int, input().rstrip().split())
 
 s = []
 def f():
-  if len(s) == M:
-    print(' '.join(map(str, s)))
+  if len(s) == M: # 2 : 길이가 만족될때 출력
+    print(' '.join(map(str, s))) # .join 함수 알기
     return
 
-  for i in range(1, N + 1):
-    if i in s:
+  for i in range(1, N + 1): # 1
+    if i in s: # 있는 숫자 pass
       continue
-    s.append(i)
-    f()
-    s.pop()
+    s.append(i) # 1 : 없는 숫자 추가
+    f() # 1.5 , 2
+    s.pop() # 3 : 출력후 이전 숫자 제거
 
 f()
 ########################################
