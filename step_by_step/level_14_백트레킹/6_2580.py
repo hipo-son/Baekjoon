33967eb315d4b2e714332fc5ca1f9fb87a917d72
hipo-son <hiposon214@gmail.com>
2022-02-19 17:04:31 +0900

    success

--- step_by_step/level_14_백트레킹/6_2580.py
+++ step_by_step/level_14_백트레킹/6_2580.py
@@ -40,39 +40,3 @@ def DFS(idx):
             Li[x][y] = 0
 
 DFS(0)
-
-
-
-
-################################################################# fail
-# num = [i for i in range(1, 10)]
-# tmp = [[] for i in range(9)]
-# row = [tmp for i in range(9)]
-# col = [tmp for i in range(9)]
-# print()
-# def rows(x) :
-#     tmp = set(num)
-#     for i in range(9) :
-#         tmp -= set([Li[x][i]])
-#     if len(tmp) != 0 :
-#         return list(tmp)
-#     else:
-#         return []
-#
-# def cols(x) :
-#     tmp = set(num)
-#     for j in range(9) :
-#         tmp -= set([Li[j][x]])
-#     if len(tmp) != 0 :
-#         return list(tmp)
-#     else:
-#         return []
-#
-# for i in range(9):
-#     for j in range(9):
-#         if Li[i][j] == 0: # 해당 좌표가 0일때
-#             row[i][j] = rows(i)
-#             col[i][j] = cols(j)
-#
-# print(row)
-# print(col)
