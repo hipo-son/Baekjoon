8585e22a3bfbb15289ee5adeac0c78b2fe09649c
hipo-son <hiposon214@gmail.com>
2022-02-20 23:33:53 +0900

    success

---
+++ etc/15666_N과 M(12).py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().rstrip().split())
+num = list(map(int, input().rstrip().split()))
+num.sort()
+result = []
+Li = []
+
+def dfs(start):
+    if len(Li) == M:
+        tmp = []
+        for i in Li:
+            tmp.append(num[i])
+        result.append(tuple(tmp))
+        return
+
+    for i in range(start, N):
+            Li.append(i)
+            dfs(i)
+            Li.pop()
+dfs(0)
+
+result = sorted(list(set(result)))
+for i in result:
+    print(*i)
