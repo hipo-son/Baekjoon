41464a86c48c0d27483d5f9cc72ca7c41b70701c
hipo-son <hiposon214@gmail.com>
2022-02-22 19:41:36 +0900

    success

--- step_by_step/level_10_재귀/2_10870.py
+++ step_by_step/level_10_재귀/2_10870.py
@@ -20,12 +20,10 @@ input = sys.stdin.readline
 N = int(input().rstrip())
 
 def fibo(x):
+    if x == 0:
+        return 0
     if x == 1 or x == 2:
         return 1
     else:
         return fibo(x-1) + fibo(x-2)
-
-if N == 0:
-    print('0')
-else:
-    print(fibo(N))
+print(fibo(N))
