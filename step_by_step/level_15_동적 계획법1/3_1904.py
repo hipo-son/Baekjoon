0e32fb79ef4de81a27ec5ea3c9e59ee8ca49fe18
hipo-son <hiposon214@gmail.com>
2022-02-23 10:19:45 +0900

    success

--- step_by_step/level_15_동적 계획법1/3_1904.py
+++ step_by_step/level_15_동적 계획법1/3_1904.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 N = int(input())
 dp = [0] * 1000001
 dp[1], dp[2] = 1, 2
-for i in range(3,N+1):
+for i in range(3, N+1):
     dp[i] = (dp[i-1] + dp[i-2])%15746
 print(dp[N])
 
