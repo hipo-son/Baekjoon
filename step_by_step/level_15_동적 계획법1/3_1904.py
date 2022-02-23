6b3d589118f7bf2966246ba668e3379baef4a97e
hipo-son <hiposon214@gmail.com>
2022-02-23 10:38:34 +0900

    success

--- step_by_step/level_15_동적 계획법1/3_1904.py
+++ step_by_step/level_15_동적 계획법1/3_1904.py
@@ -1,15 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
+N = int(input()) # 메모제이션 list bottom-up
 dp = [0] * 1000001
 dp[1], dp[2] = 1, 2
 for i in range(3, N+1):
-    dp[i] = (dp[i-1] + dp[i-2])%15746
+    dp[i] = (dp[i-1] + dp[i-2]) % 15746
 print(dp[N])
 
-# N = int(input().rstrip())
-# sys.setrecursionlimit(10**6)
+# N = int(input().rstrip()) # 메모제이션 dict top-down # 런타임 에러 (RecursionError) 발생 : Python이 정한 최대 재귀 깊이보다 커서
+# sys.setrecursionlimit(10**6) # 최대 재귀 깊이 해결 방법 이지만 실행 값이 커서 안됨
 # memo = {1: 1, 2: 2}
 # def DP(N):
 #     if N in memo:
