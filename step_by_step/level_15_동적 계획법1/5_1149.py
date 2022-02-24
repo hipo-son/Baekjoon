be00f3328c42e096a536cc661f3744f0a48cabff
hipo-son <hiposon214@gmail.com>
2022-02-25 00:28:49 +0900

    success

---
+++ step_by_step/level_15_동적 계획법1/5_1149.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+Li = [list(map(int, input().split())) for i in range(N)]
+
+for i in range(1, len(Li)):
+    Li[i][0] = min(Li[i - 1][1], Li[i - 1][2]) + Li[i][0]
+    Li[i][1] = min(Li[i - 1][0], Li[i - 1][2]) + Li[i][1]
+    Li[i][2] = min(Li[i - 1][0], Li[i - 1][1]) + Li[i][2]
+print(min(Li[N - 1][0], Li[N - 1][1], Li[N - 1][2]))
