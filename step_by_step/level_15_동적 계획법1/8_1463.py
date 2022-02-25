2ecbff70a615aff2c3ee037e458897b647b4ee67
hipo-son <hiposon214@gmail.com>
2022-02-26 01:40:46 +0900

    success

--- step_by_step/level_15_동적 계획법1/8_1463.py
+++ step_by_step/level_15_동적 계획법1/8_1463.py
@@ -7,7 +7,7 @@ memo = {1: 0, 2: 1}
 def DP(N):
     if N in memo:
         return memo[N]
-
+# 1 : 마지막 3또는 2의 경우 한번더 진행. DP(N//3) + N % 3 : 3으로 나누어지게 숫자를 추가해서 만드냐,  DP(N//2) + N % 2 : 2로 나누어지게 만드는 최소 횟수 구하기
     cnt = 1 + min(DP(N//3) + N % 3, DP(N//2) + N % 2)
     memo[N] = cnt
     return cnt
