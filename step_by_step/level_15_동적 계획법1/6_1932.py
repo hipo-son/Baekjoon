930a4d650ce4d3249552cdcbd44594ae4ad6b366
hipo-son <hiposon214@gmail.com>
2022-02-25 23:45:41 +0900

    success

--- step_by_step/level_15_동적 계획법1/6_1932.py
+++ step_by_step/level_15_동적 계획법1/6_1932.py
@@ -3,3 +3,14 @@ input = sys.stdin.readline
 
 N = int(input())
 Li = [list(map(int, input().split())) for i in range(N)]
+
+for i in range(1, N):
+    for j in range(len(Li[i])):
+        if j == 0:
+            Li[i][j] += Li[i-1][j]
+        elif j == i:
+            Li[i][j] += Li[i-1][j-1]
+        else:
+            Li[i][j] += max(Li[i-1][j], Li[i-1][j-1])
+
+print(max(Li[N-1]))
