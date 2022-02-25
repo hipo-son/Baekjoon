5997aedcd80a7bed717095aabdbe70bf8b45c9a4
hipo-son <hiposon214@gmail.com>
2022-02-26 01:12:47 +0900

    success

--- step_by_step/level_15_동적 계획법1/7_2579.py
+++ step_by_step/level_15_동적 계획법1/7_2579.py
@@ -3,10 +3,10 @@ input = sys.stdin.readline
 
 N = int(input())
 Li = [0] + [int(input()) for i in range(N)]
-DP = [0] * (N+1) # 전체
+DP = [0] * (N+1) # 총합
 
-DP[1] = Li[1]
-if N >= 2:
+DP[1] = Li[1] # 계단 한칸일때
+if N >= 2: # 두칸 이상일때
     DP[2] = max(Li[1]+Li[2], Li[2])
 for i in range(3, N+1):
     DP[i] = max(Li[i] + Li[i-1] + DP[i-3], Li[i] + DP[i-2])
