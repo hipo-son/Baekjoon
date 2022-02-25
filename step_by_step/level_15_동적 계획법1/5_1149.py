559b715344508bd96e3a7c946637fa4c0c265776
hipo-son <hiposon214@gmail.com>
2022-02-26 00:21:17 +0900

    success

--- step_by_step/level_15_동적 계획법1/5_1149.py
+++ step_by_step/level_15_동적 계획법1/5_1149.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 N = int(input())
 Li = [list(map(int, input().split())) for i in range(N)]
 
-for i in range(1, len(Li)):
+for i in range(1, len(Li)): # 0,1,2  R G B 각각의 시작 포인트
     Li[i][0] = min(Li[i - 1][1], Li[i - 1][2]) + Li[i][0]
     Li[i][1] = min(Li[i - 1][0], Li[i - 1][2]) + Li[i][1]
     Li[i][2] = min(Li[i - 1][0], Li[i - 1][1]) + Li[i][2]
