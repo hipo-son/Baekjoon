e4d1cfea9791b7ce621954794a404e5e1c88dfbc
hipo-son <hiposon214@gmail.com>
2022-02-26 20:30:19 +0900

    success

--- step_by_step/level_15_동적 계획법1/14_9251.py
+++ step_by_step/level_15_동적 계획법1/14_9251.py
@@ -4,12 +4,12 @@ input = sys.stdin.readline
 s1 = input().rstrip()
 s2 = input().rstrip()
 
-DP = [[0] * (len(s2) + 1) for i in range(len(s1) + 1)]
+DP = [[0] * (len(s2) + 1) for i in range(len(s1) + 1)] # i, j 의 DP를 i+1, j+1에 저장
 
 for i in range(len(s1)):
     for j in range(len(s2)):
-        if s1[i] == s2[j]:
+        if s1[i] == s2[j]: # 같은 글자 일때
             DP[i+1][j+1] = DP[i][j] + 1
-        else:
+        else: # 같이 않을때 주어진 문자의 최대를 저장
             DP[i+1][j+1] = max(DP[i][j+1], DP[i+1][j])
 print(DP[-1][-1])
