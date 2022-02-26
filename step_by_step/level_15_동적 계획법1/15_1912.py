5918f394ff3a9ad7f14902a01d7baa61a2970f59
hipo-son <hiposon214@gmail.com>
2022-02-26 20:46:06 +0900

    success

--- step_by_step/level_15_동적 계획법1/15_1912.py
+++ step_by_step/level_15_동적 계획법1/15_1912.py
@@ -4,4 +4,8 @@ input = sys.stdin.readline
 N = int(input())
 Li = list(map(int, input().split()))
 DP = [0] * N
-print(Li)
+DP[0] = Li[0] # N이 1일때는 DP 값이 Li[0] 과 동일
+for i in range(1, N):
+    DP[i] = max(Li[i], DP[i-1] + Li[i]) # i 번째 수가 연속적인 수열이 아닐떄, 수열일때 중 큰값을 DP 로 저장
+
+print(max(DP))
