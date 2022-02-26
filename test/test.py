40ef304f22751b4afd0803ef7418bc5f0b2900a7
hipo-son <hiposon214@gmail.com>
2022-02-26 20:25:15 +0900

    success

--- test/test.py
+++ test/test.py
@@ -1,15 +1,1 @@
-import sys
-input = sys.stdin.readline
 
-N = int(input().rstrip())
-
-def fibo(x):
-    if x == 1 or x == 2:
-        return 1
-    else:
-        return fibo(x-1) + fibo(x-2)
-
-if N == 0:
-    print('0')
-else:
-    print(fibo(N))
