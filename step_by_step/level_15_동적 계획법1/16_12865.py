87b978fb20691892a7a1cbb59c86b4cfbfe89a84
hipo-son <hiposon214@gmail.com>
2022-02-26 21:12:20 +0900

    success

--- step_by_step/level_15_동적 계획법1/16_12865.py
+++ step_by_step/level_15_동적 계획법1/16_12865.py
@@ -9,6 +9,6 @@ DP = [0] * (K+1) # 무게 0 부터 K 까지
 for _ in Li:
     W, V = map(int, _) # Li 의 W, V 값
     for j in range(K, W-1, -1):
-        DP[j] = max(DP[j], DP[j-W] + V) # 현재 DP 의 값 or 해당 W 와 V 조건일때 더 큰값 선택 
+        DP[j] = max(DP[j], DP[j-W] + V) # 현재 DP 의 값 or 해당 W 와 V 조건일때 더 큰값 선택
 
 print(DP[-1])
