c07a4b7b8715ef187a68be70b19ce1ea7b5b50a2
hipo-son <hiposon214@gmail.com>
2022-03-14 21:12:56 +0900

    success

--- step_by_step/level_18_스택/5_1874.py
+++ step_by_step/level_18_스택/5_1874.py
@@ -1,1 +1,23 @@
+import sys
+input = sys.stdin.readline
 
+N = int(input())
+stack, Li = [], []
+count = 1; temp = 1
+for i in range(N):
+    num = int(input())
+    while count <= num:
+        stack.append(count)
+        Li.append('+')
+        count += 1
+    if stack[-1] == num:
+        stack.pop()
+        Li.append('-')
+    else:
+        temp = 0
+
+if not temp:
+    print('NO')
+else:
+    for _ in Li:
+        print(_)
